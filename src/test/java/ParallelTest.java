695b13c279ce0993c20dfaf4c7a9f8572c36c43b
tedongol <teena.dongol777@gmail.com>
2018-07-28 11:46:33 +1000

    Imlicit removed and firefox issue solved

--- src/test/java/ParallelTest.java
+++ src/test/java/ParallelTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -38,8 +39,9 @@ public class ParallelTest {
     @Test
     public void Addtocart() throws InterruptedException {
     	WebDriverWait wait = new WebDriverWait(driver, 15);
-    	driver.findElement(By.id("gh-ac")).sendKeys("adidas");
+    	driver.findElement(By.id("gh-ac")).sendKeys("Adidas Neo");
     	driver.findElement(By.id("gh-btn")).click();
+    	wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//*[@id=\"srp-river-results-listing1\"]/div/div[2]/a/h3")));
         String product = driver.findElement(By.xpath("//*[@id=\"srp-river-results-listing1\"]/div/div[2]/a/h3")).getText();
     	System.out.println(product);
     	
@@ -48,6 +50,7 @@ public class ParallelTest {
     	String window1=driver.getWindowHandle();
     	driver.switchTo().window(window1);
     	System.out.println(window1);
+    	wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//*[@id=\"atcRedesignId_overlay-atc-container\"]/div/div[1]/div/div[2]/a[2]/span/span")));
         driver.findElement(By.xpath("//*[@id=\"atcRedesignId_overlay-atc-container\"]/div/div[1]/div/div[2]/a[2]/span/span")).click();
     	String productcart = driver.findElement(By.xpath("//*[@id=\"142236394587_title\"]/a")).getText();
     	System.out.println(productcart);
@@ -61,11 +64,18 @@ public class ParallelTest {
     	String window2=driver.getWindowHandle();
     	driver.switchTo().window(window2);
     	System.out.println(window2);
+    	wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//*[@id=\"atcRedesignId_overlay-atc-container\"]/div/div[1]/div/div[2]/a[2]/span/span")));
     	driver.findElement(By.xpath("//*[@id=\"atcRedesignId_overlay-atc-container\"]/div/div[1]/div/div[2]/a[2]/span/span")).click();
     	String productcart1 = driver.findElement(By.xpath("//*[@id=\"172731601888_title\"]/a")).getText();
     	System.out.println(productcart1);
     	Assert.assertEquals(product1, productcart1);
     	Assert.assertEquals(product, productcart);
+    	driver.findElement(By.id("gh-cart-n")).click();
+    	String cartNum = driver.findElement(By.id("gh-cart-n")).getText();
+    	Assert.assertEquals(cartNum, "2");
+    	System.out.println("First product added in the cart: "+product1);
+    	System.out.println("Second product added in the cart: "+product);
+    	System.out.println("Items  added in the cart ="+cartNum);
     }
 
     @AfterClass public void afterTest() {
